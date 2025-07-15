c51c3b4a50e8749f20e9fd109914a164048bde75
Anastasiia Atanasevych <nuistia@gmail.com>
2025-07-15 14:34:16 +0300

    Setup projects

---
+++ Accountino.Api/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Accountino.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        // ТODO: Register Application layer services
+
+        return services;
+    }
+
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        // TODO: Register Infrastructure layer services
+
+        return services;
+    }
+}
