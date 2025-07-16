013862a78ae8c1d31b4e127854871e207b7ec8e2
Anastasiia Atanasevych <nuistia@gmail.com>
2025-07-16 14:53:20 +0300

    Add repositories

---
+++ Accountino.Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using Accountino.Domain.Common;
+using Accountino.Domain.Common.Specification;
+
+namespace Accountino.Application.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : IEntity
+{
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+
+    Task<TEntity> GetByIdAsync(int id);
+    Task<List<TEntity>> GetBySpecification(Specification<TEntity> spec);
+}
