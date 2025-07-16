a6ee03e94380943892cc2df00b7e3b201c764038
Anastasiia Atanasevych <nuistia@gmail.com>
2025-07-16 17:08:59 +0300

    Simplify collection initializer

--- Accountino.Domain/Common/Specification/Specification.cs
+++ Accountino.Domain/Common/Specification/Specification.cs
@@ -6,7 +6,7 @@ public class Specification<T> : ISpecification<T> where T : IEntity
 {
     public Expression<Func<T, bool>> Criteria { get; init; } = _ => true;
 
-    public List<Expression<Func<T, object>>> Includes { get; } = new();
+    public List<Expression<Func<T, object>>> Includes { get; } = [];
 
     protected void AddInclude(Expression<Func<T, object>> includeExpression)
     {
