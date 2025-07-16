967e15419f73b20d80f0c530d5d8c5ceb6824609
Anastasiia Atanasevych <nuistia@gmail.com>
2025-07-16 13:21:54 +0300

    Create EF models

---
+++ Accountino.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using Accountino.Domain.Common;
+
+namespace Accountino.Domain.Entities;
+
+public class Category : IEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public ICollection<Expense> Expenses { get; set; }
+}
---
+++ Accountino.Domain/Entities/Expense.cs
@@ -0,0 +1,13 @@
+﻿using Accountino.Domain.Common;
+
+namespace Accountino.Domain.Entities;
+
+public class Expense : IEntity
+{
+    public int Id { get; set; }
+    public double Price { get; set; }
+    public string? Description { get; set; }
+
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
